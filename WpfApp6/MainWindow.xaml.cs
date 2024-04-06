aeb6339e34e5d6d6bc353ba72f0775d22c644486
darkqem <darkqem@gmail.com>
2024-04-06 09:18:22 +0500

    Добавьте файлы проекта.

---
+++ WpfApp6/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics.PerformanceData;
+using System.IO;
+using System.Windows;
+
+namespace WpfApp6
+{
+    public partial class MainWindow : Window
+    {
+        private int counter = 0;
+        private const string FileName = "counter.txt";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadCounterValue();
+        }
+
+        private void CounterButton_Click(object sender, RoutedEventArgs e)
+        {
+            counter++;
+            CounterText.Text = $"Нажатий: {counter}";
+            SaveCounterValue();
+        }
+
+        private void LoadCounterValue()
+        {
+            if (File.Exists(FileName))
+            {
+                string value = File.ReadAllText(FileName);
+                if (int.TryParse(value, out int loadedCounter))
+                {
+                    counter = loadedCounter;
+                    CounterText.Text = $"Нажатий: {counter}";
+                }
+            }
+        }
+
+        private void SaveCounterValue()
+        {
+            File.WriteAllText(FileName, counter.ToString());
+        }
+    }
+}
